bba657b234a431828b09800a254de1393313e2ad
Hands-On Lab User <holuser@docker.oracleworld.com>
2018-10-02 15:00:27 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 8081;
+
+app.get('/status', function (req, res) {
+        res.set('Content-Type', 'application/json');
+        res.end('{"status": {"message": "Hi! All working here!"}}');
+})
+
+app.get('/sum', function (req, res) {
+        var num1 = parseInt(req.query.num1);
+        var num2 = parseInt(req.query.num2);
+        var result = num1 + num2;
+        res.set('Content-Type', 'application/json');
+        res.end('{"result": {"operation": "' +num1+"+"+num2+'", "result":'+result+'}}');
+})
+
+server = app.listen(PORT, function () {
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("The magic happens here! Server listening at http://%s:%s", host, port)
+
+
+})
